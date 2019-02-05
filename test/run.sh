4ee96cbcb92664074edbd7a9f28bbc3d84132a10
BAHAR ALIPANAHI <bahar.panahie@gmail.com>
2019-02-05 12:46:34 -0800

    20,000 reads, high -ci

--- test/run.sh
+++ test/run.sh
@@ -4,6 +4,8 @@ mkdir -p kmc_temp
 ./kmc -b -ci12 -fq -k32 -cs250 new.fastq list.kmc kmc_temp
 ./kmc_tools sort list.kmc list.kmc.sorted
 
+echo "list.kmc.sorted" > list
+
 #construct the de Bruijn graph
 ../cosmo-pack -k list
 
