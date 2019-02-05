830423b168de8f456cfa81f109134ee30e1fcc41
BAHAR ALIPANAHI <bahar.panahie@gmail.com>
2019-02-04 21:40:35 -0800

    20,000 reads with high -ci

---
+++ test/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#count the k-mers
+mkdir -p kmc_temp
+./kmc -b -ci12 -fq -k32 -cs250 new.fastq list.kmc kmc_temp
+./kmc_tools sort list.kmc list.kmc.sorted
+
+#construct the de Bruijn graph
+../cosmo-pack -k list.kmc.sorted
+
+#construct the read-colored matrix with reduced number of colors:
+../reduce_color new.fastq 20000
