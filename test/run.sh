2f0dc7dff82eb05d9929b148e32ba7ae33bd238b
BAHAR ALIPANAHI <bahar.panahie@gmail.com>
2019-04-12 16:56:23 -0700

    cosmo independent

--- test/run.sh
+++ test/run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #count the k-mers
-mkdir -p kmc_temp
-./kmc -b -ci12 -fq -k32 -cs250 new.fastq list.kmc kmc_temp
-./kmc_tools sort list.kmc list.kmc.sorted
+mkdir -p  kmc_temp
+./kmc -b -ci3 -fq -k32 -cs250 new.fastq list.kmc kmc_temp
+./kmc_dump -ci3  list.kmc dump
 
-echo "list.kmc.sorted" > list
-
-#construct the de Bruijn graph
-../cosmo-pack -k list
+#change the format of file called dump
+python ../edit.py dump pair
 
 #construct the read-colored matrix with reduced number of colors:
-../reduce_color new.fastq 20000
+../reduce_color new.fastq 20000 32
